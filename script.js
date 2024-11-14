91880058c0a675c88d4d14106aa441c2e0b5590f
Tracy Njoroge <tracywangari997@gmail.com>
2024-11-14 21:05:04 +0300

    update script.js

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 function free() {
     let name = prompt("What is your Name?");
     let email = prompt("What is your email address?");
-
-    alert(
+    if (name === "") {
+      alert("Invalid! Enter your name.")
+    } else if( email === "") {
+      alert("Invalid! Enter your email.")
+    } else {alert(
       "Hello🖐 " +
         name +
         ", Thank you for being interested, We'll be in touch!"
-    );
+    )
+  }
+
   }
 
   let bookButton = document.querySelector("button");
